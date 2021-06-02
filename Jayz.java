d33ee29a1c5e25efedd9ef536e2d701264bc8e9c
Dyong2ya <84007079+Dyong2ya@users.noreply.github.com>
2021-06-02 18:51:51 +0900

    Update Jayz.java

--- Jayz.java
+++ Jayz.java
@@ -15,23 +15,23 @@ public class Jayz {
   this.weight = this.maxWeight;
   this.foods = foods;
  }
- void getWeight(int weight){//
-	 this.weight += weight;//
+//  void getWeight(int weight){//
+// 	 this.weight += weight;//
 	
-	 }
+// 	 }
  
- void getFood(Food food){
-	   System.out.println(food.name + " 을/를 잃었어요ㅜㅜ");//
-	   System.out.println();
-	   for(int i = 0; i < foods.length; i++){
-	    if(foods[i] == null){
-	     foods[i] = food;
-	     break;
-	     }
-	   }
-	   weight += food.weight; //
+//  void getFood(Food food){
+// 	   System.out.println(food.name + " 을/를 잃었어요ㅜㅜ");//
+// 	   System.out.println();
+// 	   for(int i = 0; i < foods.length; i++){
+// 	    if(foods[i] == null){
+// 	     foods[i] = food;
+// 	     break;
+// 	     }
+// 	   }
+// 	   weight += food.weight; //
 	   
-	  }
+// 	  }
 	   Food foodDrop(){
 			return foods[(int)(Math.random() * foods.length)];
 
